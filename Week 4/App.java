1efcaa4c9e4aaaf7e07470af2c6edac2d8eda3e6
carrke <carrke@merrimack.edu>
2024-10-02 17:51:18 -0400

    added week 4 assignment

---
+++ Week 4/App.java
@@ -0,0 +1,21 @@
+/**
+ * Abstract class for application execution. Application should repeatedly ask user for integer information 
+ * and print out a sorted list of that information when completed.
+ * <br><br>
+ * run(): Run the application
+ * 
+ * @author Kevin Carr
+ * @version 1.0.0
+ * @since CSC6301 Week 4
+ */
+public abstract class App {
+    /**
+     * 
+     */
+    protected DataStore dataStore;
+    protected DataRequester dataRequester;
+    /**
+     * Method to run application once prepared, should be implemented by concrete subclasses
+     */
+    public abstract void run();
+}
---
+++ Week 4/DataStore.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+/**
+ * Interface for integer data storage object <br><br>
+ * add() to add int data to DataStore <br>
+ * sort() to sort data from lowest to highest <br>
+ * get_data() to provide data as a LinkedList
+ * 
+ * @author Kevin Carr
+ * @version 1.0.0
+ * @since CSC6301 Week 4
+ */
+public interface DataStore {
+    /**
+     * Add int/integer to DataStore
+     * @param n int to add
+     */
+    public void add(int n);
+    /**
+     * Ensure stored data is sorted
+     */
+    public void sort();
+    /**
+     * Provide stored data to requester
+     * @return LinkedList<Integer>
+     */
+    public LinkedList<Integer> get_data();
+}
